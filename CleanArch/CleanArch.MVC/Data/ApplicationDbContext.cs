6078cc02d83bd4ccbd0d722f4454f9d8a34e6abc
Javad Malekzadeh <javad_malekzadeh@live.com>
2020-02-27 20:12:17 +0330

    Add MVC to Presentation layer

---
+++ CleanArch/CleanArch.MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using CleanArch.MVC.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.MVC.Data
+{
+    public class ApplicationDbContext
+    {
+        private readonly IMongoDatabase _database = null;
+
+        public ApplicationDbContext(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            if (client != null)
+                _database = client.GetDatabase(settings.Value.Database);
+        }
+
+        //public IMongoCollection<Note> Notes
+        //{
+        //    get
+        //    {
+        //        return _database.GetCollection<Note>("Note");
+        //    }
+        //}
+    }
+}
---
+++ CleanArch/CleanArch.MVC/Models/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.MVC.Models
+{
+    public class Settings
+    {
+        public string ConnectionString;
+        public string Database;
+    }
+}
