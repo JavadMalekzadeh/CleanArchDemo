81a49a06c119983d5a3ea5dad3d11e54ba240f3f
Javad Malekzadeh <javad_malekzadeh@live.com>
2020-03-03 22:13:36 +0330

    inittial context model

---
+++ CleanArch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class Setting
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+    }
+    public interface IGameContext
+    {
+       
+        IMongoCollection<Game> Games { get; }
+    }
+    class UniversityDBContext
+    {
+
+    }
+}
